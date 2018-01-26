d90805de0eb4f1669842654d9816b5dc8025f909
Daniel Nilsson <daniel.nilsson94@outlook.com>
2018-01-26 20:44:24 +0100

    Initial commit

---
+++ Eventkalender.PK/Model/Event.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventkalender.Model
+{
+    public class Event
+    {
+        private string name;
+        private string description;
+
+        private DateTime startTime;
+        private DateTime endTime;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public DateTime StartTime
+        {
+            get { return startTime; }
+            set { startTime = value; }
+        }
+
+        public DateTime EndTime
+        {
+            get { return endTime; }
+            set { endTime = value; }
+        }
+    }
+}

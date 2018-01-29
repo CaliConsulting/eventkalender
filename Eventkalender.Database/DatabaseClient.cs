78f01c498fbf77f11076c1a89bb79187f5726258
Daniel Nilsson <daniel.nilsson94@outlook.com>
2018-01-29 14:49:41 +0100

    Added basic database functionality

---
+++ Eventkalender.Database/DatabaseClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Eventkalender.Database
+{
+    public class DatabaseClient
+    {
+        public static SqlConnection GetConnection(string xmlPath)
+        {
+            Dictionary<string, string> values = ReadXmlFile(xmlPath);
+            string dataSource = values["DataSource"];
+            string database = values["Database"];
+            string username = values["Username"];
+            string password = values["Password"];
+
+            String connectionString = String.Format("Data Source={0};Initial Catalog={1};User Id={2};Password={3};", dataSource, database, username, password);
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+
+        public static Dictionary<string, string> ReadXmlFile(string xmlPath)
+        {
+            XDocument doc = XDocument.Load(xmlPath);
+            Dictionary<string, string> xmlValues = new Dictionary<string, string>();
+            xmlValues.Add("DataSource", doc.Root.Element("DataSource").Value);
+            xmlValues.Add("Database", doc.Root.Element("Database").Value);
+            xmlValues.Add("Username", doc.Root.Element("Username").Value);
+            xmlValues.Add("Password", doc.Root.Element("Password").Value);
+            return xmlValues;
+        }
+    }
+}
---
+++ Eventkalender.PK/Program.cs
@@ -0,0 +1,31 @@
+﻿using Eventkalender.Database;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventkalender.PK
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //DatabaseClient client = new DatabaseClient("eventkalender-db.xml");
+
+            string d = Directory.GetCurrentDirectory();
+            Console.WriteLine(d);
+            Console.Read();
+
+            using (SqlConnection c = DatabaseClient.GetConnection("eventkalender-db.xml"))
+            {
+                string version = c.ServerVersion;
+                Console.WriteLine("V: " + version);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

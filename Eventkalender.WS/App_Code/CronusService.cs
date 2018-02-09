f56c8c1082fe655250434e6cb3863f74bf4489a4
Philip <35528685+PhilipEriksson@users.noreply.github.com>
2018-02-09 12:36:32 +0100

    CronusWS Fix
    Implementerat metodkroppar i väntan på att queries samt CronusController.

---
+++ Eventkalender.WS/App_Code/CronusService.cs
@@ -0,0 +1,52 @@
+﻿using Eventkalender.Database.DAL;
+using Eventkalender.Database.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Eventkalender.Database.Controller;
+
+
+
+[WebService(Namespace = "http://www.ics.lu.se.cali/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+
+
+public class CronusService : WebService
+{
+
+    private CronusController cronsusController;
+    public CronusService()
+    {
+        cronusController = new CronusController();
+    }
+
+    [WebMethod]
+    public void Update()
+    {
+
+        return cronusController.Update();
+    }
+    [WebMethod]
+    public void Insert()
+    {
+
+        return cronusController.Insert();
+    }
+    [WebMethod]
+    public void Delete()
+    {
+
+        return cronusController.Delete();
+    }
+    [WebMethod]
+    public void Select()
+    {
+
+        return cronusController.Select();
+    }
+}

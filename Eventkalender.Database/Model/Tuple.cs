ee44b7b01465a9af42742b7fd8b60732084a9b9f
Daniel Nilsson <daniel.nilsson94@outlook.com>
2018-02-12 11:35:52 +0100

    Added tuple model

---
+++ Eventkalender.Database/Model/Tuple.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventkalender.Database.Model
+{
+    public class Tuple
+    {
+        private Dictionary<string, string> values;
+
+        public Tuple()
+        {
+            values = new Dictionary<string, string>();
+        }
+
+        public void Add(string column, string value)
+        {
+            values.Add(column, value);
+        }
+
+        public Dictionary<string, string>.KeyCollection Columns
+        {
+            get { return values.Keys; }
+            private set { }
+        }
+
+        public Dictionary<string, string>.ValueCollection Values
+        {
+            get { return values.Values; }
+            private set { }
+        }
+    }
+}

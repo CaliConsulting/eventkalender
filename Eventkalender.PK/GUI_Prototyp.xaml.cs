c313991197d98ecfe283c9b4561fd9ea3c59637b
h4xed <nek15hsa@student.lu.se>
2018-02-14 16:03:52 +0100

    tidslistan fuckar mig.

--- Eventkalender.PK/GUI_Prototyp.xaml.cs
+++ Eventkalender.PK/GUI_Prototyp.xaml.cs
@@ -21,16 +21,54 @@ namespace Eventkalender.PK.GUI
     public partial class GUI_Prototyp : Window
     {
         EventkalenderController controller = new EventkalenderController("Resources/eventkalender-db.xml");
-        List<string> timeList = new List<string> {"00:00", "00:30", "01;00", "01;30", "02:00", "02:30", "03;00", "03;30", "04:00", "04:30", "05;00", "05;30",
-                                                           "06:00", "06:30", "07;00", "07;30","08:00", "08:30", "09;00", "09;30","10:00", "10:30", "11;00", "11;30",
-                                                           "12:00", "12:30", "13;00", "13;30","14:00", "14:30", "15;00", "15;30","16:00", "16:30", "17;00", "17;30",
-                                                           "18:00", "18:30", "19;00", "19;30","20:00", "20:30", "21;00", "21;30","22:00", "22:30", "23;00", "23;30"};
         public GUI_Prototyp()
         {
             InitializeComponent();
         }
-
-
+        public List<string> GenerateList()
+        {
+            List<string> times = new List<string>();
+            for (int i = 0; i < 48; i++)
+            {
+                int hour = (int)Math.Floor(i / 2d);
+                string strHour = hour.ToString();
+                if (hour < 10)
+                {
+                    strHour = "0" + strHour;
+                }
+                string res = strHour + ":";
+                if (i % 2 == 1)
+                {
+                    res += "3";
+                }
+                else
+                {
+                    res += "0";
+                }
+                res += "0";
+                times.Add(res);
+            }
+            return times;
+        }
+        public List<string> timesList
+        {
+            get
+            {
+                if (timesList == null)
+                {
+                    timesList = GenerateList();
+                    return timesList;
+                }
+                else
+                {
+                    return timesList;
+                }
+            }
+            set
+            {
+                timesList = GenerateList();
+            }
+        }
         private void SearchBox_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
 
@@ -71,12 +109,12 @@ namespace Eventkalender.PK.GUI
 
             }
         }
-        private void cmb_SortEvents_Selected(object sender, RoutedEventArgs e)
+/*        private void cmb_SortEvents_Selected(object sender, RoutedEventArgs e)
         {
             if(cmb_SortEvents.SelectedValue == Datum)
             {
                 
             }
-        }
+        }*/
     }
 }

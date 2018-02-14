99636599d0143039344d11d07a8a9f9dd1f507a1
h4xed <nek15hsa@student.lu.se>
2018-02-14 12:58:33 +0100

    I can't develop just yet.

--- Eventkalender.PK/GUI_Prototyp.xaml.cs
+++ Eventkalender.PK/GUI_Prototyp.xaml.cs
@@ -20,16 +20,18 @@ namespace Eventkalender.PK.GUI
     /// </summary>
     public partial class GUI_Prototyp : Window
     {
+        EventkalenderController controller = new EventkalenderController("Resources/eventkalender-db.xml");
+        List<string> timeList = new List<string> {"00:00", "00:30", "01;00", "01;30", "02:00", "02:30", "03;00", "03;30", "04:00", "04:30", "05;00", "05;30",
+                                                           "06:00", "06:30", "07;00", "07;30","08:00", "08:30", "09;00", "09;30","10:00", "10:30", "11;00", "11;30",
+                                                           "12:00", "12:30", "13;00", "13;30","14:00", "14:30", "15;00", "15;30","16:00", "16:30", "17;00", "17;30",
+                                                           "18:00", "18:30", "19;00", "19;30","20:00", "20:30", "21;00", "21;30","22:00", "22:30", "23;00", "23;30"};
         public GUI_Prototyp()
         {
             InitializeComponent();
         }
+
+
         private void SearchBox_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
-        {
-
-        }
-
-        private void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
         {
 
         }
@@ -68,13 +70,13 @@ namespace Eventkalender.PK.GUI
             {
 
             }
-
-
         }
-
-        private void cmBox_SortEvents_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void cmb_SortEvents_Selected(object sender, RoutedEventArgs e)
         {
-
+            if(cmb_SortEvents.SelectedValue == Datum)
+            {
+                
+            }
         }
     }
 }

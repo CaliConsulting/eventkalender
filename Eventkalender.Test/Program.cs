4b018f43fbe61f6691bf7d816e3ec072fe5ef98a
Daniel Nilsson <daniel.nilsson94@outlook.com>
2018-02-16 13:36:00 +0100

    Testar saker

---
+++ Eventkalender.Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using Eventkalender.Database;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Eventkalender.Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            EventkalenderController c = new EventkalenderController("Resources/eventkalender-db.xml");
+
+            Stopwatch s = new Stopwatch();
+
+            s.Start();
+            List<Event> events = c.GetEvents();
+            s.Stop();
+
+            Console.WriteLine(s.ElapsedMilliseconds);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Eventkalender.WS/App_Code/Initialization.cs
@@ -0,0 +1,22 @@
+﻿using Eventkalender.Database;
+using Eventkalender.WS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace Eventkalender.WS
+{
+    public class Initialization : IProcessHostPreloadClient
+    {
+        public void Preload(string[] parameters)
+        {
+            string eventkalenderDatabaseFilePath = PathUtility.GetPhysicalPath("~/App_Data") + "/eventkalender-db.xml";
+            using (EventkalenderContext context = new EventkalenderContext(eventkalenderDatabaseFilePath))
+            {
+                context.Nation.FirstOrDefault();
+            }
+        }
+    }
+}
